cac4e828e3e14bdee0d542626bded1935a9d01bc
Maria Eduarda Parahyba <eduardaparahyba@gmail.com>
2020-08-16 18:40:51 -0300

    Adding some tests

--- src/test/java/school/cesar/eta/unit/PersonTest.java
+++ src/test/java/school/cesar/eta/unit/PersonTest.java
@@ -1,43 +1,83 @@
 package school.cesar.eta.unit;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
+import java.time.LocalDate;
 
 import static org.junit.jupiter.api.Assertions.fail;
 
 public class PersonTest {
+
+    private Person person;
+
+    @BeforeEach
+    public void  setupPerson(){
+        person = new Person();
+    }
+
     @Test
-    public void getName_firstNameJonLastNameSnow_jonSnow() {
-        fail();
+    public void getName_firstNameJonLastNameSnow_jonSnow(){
+        person.setName("Jon");
+        person.setLastName("Snow");
+        Assertions.assertEquals("JonSnow",person.getName());
     }
 
     @Test
     public void getName_firstNameJonNoLastName_jon() {
-        fail();
+        person.setName("Jon");
+        Assertions.assertEquals("Jon",person.getName());
     }
 
     @Test
     public void getName_noFirstNameLastNameSnow_snow() {
-        fail();
+        person.setLastName("Snow");
+        Assertions.assertEquals("Snow",person.getName());
     }
 
     @Test
     public void getName_noFirstNameNorLastName_throwsException() {
-        fail();
+        Assertions.assertThrows(RuntimeException.class, () -> person.getName());
     }
 
     @Test
     public void isBirthdayToday_differentMonthAndDay_false() {
-        fail();
+        person = new Person(){
+            @Override
+            public LocalDate getNow() {
+                LocalDate dateNow = LocalDate.now();
+                return LocalDate.of(dateNow.getYear(),dateNow.getMonthValue()+4,dateNow.getDayOfMonth()+3);
+            }
+        };
+        person.setBirthday(LocalDate.now());
+        Assertions.assertFalse(person.isBirthdayToday());
     }
 
     @Test
     public void isBirthdayToday_sameMonthDifferentDay_false() {
-        fail();
+        person = new Person(){
+            @Override
+            public LocalDate getNow() {
+                LocalDate dateNow = LocalDate.now();
+                return LocalDate.of(dateNow.getYear(),dateNow.getMonthValue(),dateNow.getDayOfMonth()-6);
+            }
+        };
+        person.setBirthday(LocalDate.now());
+        Assertions.assertFalse(person.isBirthdayToday());
     }
 
     @Test
     public void isBirthdayToday_sameMonthAndDay_true() {
-        fail();
+        person = new Person(){
+            @Override
+            public LocalDate getNow() {
+                LocalDate dateNow = LocalDate.now();
+                return LocalDate.of(dateNow.getYear(),dateNow.getMonthValue(),dateNow.getDayOfMonth());
+            }
+        };
+        person.setBirthday(LocalDate.now());
+        Assertions.assertTrue(person.isBirthdayToday());
     }
 
     @Test
