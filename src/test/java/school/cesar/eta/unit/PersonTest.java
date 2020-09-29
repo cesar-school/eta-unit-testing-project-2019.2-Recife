5c885de4f63ee2987fb2c764f2b025755f5acc07
Danilo <daniloferreira2004@gmail.com>
2020-09-29 12:32:41 -0300

    resolvendo os retornos projeto final

--- src/test/java/school/cesar/eta/unit/PersonTest.java
+++ src/test/java/school/cesar/eta/unit/PersonTest.java
@@ -3,45 +3,54 @@ package school.cesar.eta.unit;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Spy;
+import org.mockito.junit.jupiter.MockitoExtension;
 
-import java.rmi.UnexpectedException;
 import java.time.LocalDate;
+import java.util.List;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.fail;
+import static org.mockito.Mockito.*;
 
+@ExtendWith(MockitoExtension.class)
 public class PersonTest {
 
+    private Person person;
+
+    @InjectMocks
+    private Person personOne;
+    @InjectMocks
+    private Person personTwo;
+    @Spy
+    private List<Person> family;
 
     @BeforeEach
     public void setupTest() {
-        person = new Person();
-    };
-
-    private Person person;
-    private Person personOne;
-    private Person personTwo;
+        person = new Person() {
+            @Override
+            public LocalDate getNow() {
+                return LocalDate.of(2020, 9, 2);
+            }
+        };
+    }
 
     @Test
     public void getName_firstNameJonLastNameSnow_jonSnow()  {
         person.setName("Jon");
         person.setLastName("Snow");
-        Assertions.assertEquals("Jon",person.getFirstName());
-        Assertions.assertEquals("Snow",person.getLastName());
         Assertions.assertEquals("JonSnow", person.getName());
     }
 
     @Test
     public void getName_firstNameJonNoLastName_jon() {
         person.setName("Jon");
-        Assertions.assertEquals("Jon",person.getFirstName());
         Assertions.assertEquals("Jon",person.getName());
     }
 
     @Test
     public void getName_noFirstNameLastNameSnow_snow() {
         person.setLastName("Snow");
-        Assertions.assertEquals("Snow",person.getLastName());
         Assertions.assertEquals("Snow",person.getName());
     }
 
@@ -52,42 +61,35 @@ public class PersonTest {
 
     @Test
     public void isBirthdayToday_differentMonthAndDay_false() {
-        person.setBirthday(LocalDate.parse("1988-09-02"));
-        Assertions.assertNotEquals(person.getBirthday().getMonth(),LocalDate.now().getMonth());
-        Assertions.assertNotEquals(person.getBirthday().getDayOfMonth(),LocalDate.now().getDayOfMonth());
+        LocalDate birthday = LocalDate.of(1988,8, 5);
+        person.setBirthday(birthday);
         Assertions.assertFalse(person.isBirthdayToday());
     }
 
     @Test
     public void isBirthdayToday_sameMonthDifferentDay_false() {
-        person.setBirthday(LocalDate.parse("1988-08-02"));
-        Assertions.assertEquals(person.getBirthday().getMonth(),LocalDate.now().getMonth());
-        Assertions.assertNotEquals(person.getBirthday().getDayOfMonth(),LocalDate.now().getDayOfMonth());
+        LocalDate birthday = LocalDate.of(1988,7, 2);
+        person.setBirthday(birthday);
         Assertions.assertFalse(person.isBirthdayToday());
     }
 
     @Test
     public void isBirthdayToday_sameMonthAndDay_true() {
-        person.setBirthday(LocalDate.parse("1988-08-14"));
-        Assertions.assertEquals(person.getBirthday().getMonth(),LocalDate.now().getMonth());
-        Assertions.assertEquals(person.getBirthday().getDayOfMonth(),LocalDate.now().getDayOfMonth());
+        LocalDate birthday = LocalDate.of(1988,9, 2);
+        person.setBirthday(birthday);
         Assertions.assertTrue(person.isBirthdayToday());
     }
 
     @Test
     public void addToFamily_somePerson_familyHasNewMember() {
-        personOne = new Person();
-        personTwo = new Person();
         personOne.addToFamily(personTwo);
-        Assertions.assertTrue(personOne.isFamily(personTwo));
+        verify(family, times(1)).add(personTwo);
     }
 
     @Test
     public void addToFamily_somePerson_personAddedAlsoHasItsFamilyUpdated() {
-        personOne = new Person();
-        personTwo = new Person();
-        personTwo.addToFamily(personOne);
-        Assertions.assertTrue(personTwo.isFamily(personOne));
+        personOne.addToFamily(personTwo);
+        verify(family, times(1)).add(personOne);
     }
 
     @Test
@@ -98,8 +100,7 @@ public class PersonTest {
 
     @Test
     public void isFamily_relativePerson_true() {
-        personTwo = new Person();
-        person.addToFamily(personTwo);
-        Assertions.assertTrue(person.isFamily(personTwo));
+        when(family.contains(null)).thenReturn(true);
+        Assertions.assertTrue(personOne.isFamily(null));
     }
 }
