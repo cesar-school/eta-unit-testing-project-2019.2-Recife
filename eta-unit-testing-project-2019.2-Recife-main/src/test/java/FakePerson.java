927fcc15ae80937ea39ab570095e9d228f5811eb
edimailza <celular.corporativo.edi@gmail.com>
2020-09-30 18:48:56 -0300

    [Versão final] Projeto corrigido, commitado e ajustado.

--- eta-unit-testing-project-2019.2-Recife-main/src/test/java/FakePerson.java
+++ eta-unit-testing-project-2019.2-Recife-main/src/test/java/FakePerson.java
@@ -1,16 +1,13 @@
 
-
 import school.cesar.eta.unit.Person;
 
-public class FakePerson extends Person{
-	
-	@Override
-	 public boolean isFamily(Person person) {
-		return true;
-	}
-	
-//	@Override
-//	public LocalDate getNow() {
-//        return LocalDate.;
-//    }
+public class FakePerson extends Person
+{
+
+    @Override
+    public boolean isFamily(Person person)
+    {
+        return true;
+    }
+
 }
